e972b0fe1a876cd729e27fb7316eb16f0f6b0340
Quyen Tran <qtrandev@gmail.com>
2016-05-15 01:36:31 -0400

    Add basic user profile layout

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -5,18 +5,35 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
   TouchableHighlight
 } from 'react-native';
 
 import {Actions} from 'react-native-router-flux';
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: 'Quyen Tran'
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Profile
-        </Text>
+        <View style={styles.userView}>
+          <Image
+            style={styles.userIcon}
+            source={{uri: 'https://pbs.twimg.com/profile_images/692594933488848896/uDJQ6oFm.jpg'}}
+          />
+          <Text style={styles.userName}>
+            {this.state.userName}
+          </Text>
+          <Image
+          style={styles.userRating}
+          source={{uri: 'http://ridesharegenius.com/wp-content/uploads/2015/03/5-stars-640x162.jpg'}}
+          />
+        </View>
         <TouchableHighlight
             style={styles.button}
             onPress={Actions.login}
@@ -41,14 +58,30 @@ class Profile extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: 'column',
+    marginTop: 60,
     backgroundColor: '#123456',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
+  userView: {
+    flex: 1,
     margin: 10,
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  userIcon: {
+    width: 50,
+    height: 50,
+    borderWidth: 0.5,
+    borderColor: 'black'
+  },
+  userName: {
+    fontSize: 15,
+    color: 'white',
+    padding: 5
+  },
+  userRating: {
+    width: 150,
+    height: 30
   },
   button: {
     height: 36,
